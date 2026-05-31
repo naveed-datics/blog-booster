5252f271f70292d74b85ea03b501f6a839cd290c
Apple <apple@MacBook-Pro.local>
2026-05-31 17:32:32 +0500

    remove crone

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  experimental: {
-    instrumentationHook: true, // Enable instrumentation hook for cron initialization
-  },
 };
 
 export default nextConfig;
